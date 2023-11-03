a57b415fd10cfdfc99319d3230173a13efa26c9f
Nikhil Pradip More <nikupcm10@gmail.com>
2023-11-03 22:27:30 +0530

    swap_array_final

--- reverse_array/swap_array.java
+++ reverse_array/swap_array.java
@@ -1,5 +1,36 @@
 package reverse_array;
 
+import java.util.Scanner;
+
 public class swap_array {
-    
+    static void reverseArray(int arr[], int n){
+        int p1 = 0, p2 = n-1;
+
+        while(p1<p2){
+            int tmp = arr[p1];
+            arr[p1] = arr[p2];
+            arr[p2] = tmp;
+            p1++;
+            p2--;
+
+        }
+        for(int i = 0; i<n; i++){
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+        System.out.println("enter array index " + i);
+        arr[i] = sc.nextInt();
+        }
+        reverseArray(arr, n);
+        
+
+sc.close();
+    }
 }
